99033f4d29dffc6a19780cbe8afbe33cca3f4e78
teoyeejay-oss <teoyeejay@gmail.com>
2026-04-11 22:09:47 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#string
+name='Teo yee jay'
+print(f'my name is {name}')
+school_name='tarumt'
+print(f"now I am study in {school_name}")
+
+#integer
+height=171
+print(f'And my height is {height}cm')
+
+
+#float
+average_score=41.5
+print(f"My average score in secondary school is {average_score}")
+
+
+#boolean
+is_cs_student = False
+if is_cs_student:
+    print('I am also is a cs student')
+else:
+    print('But I am not a cs student')
+
